6d2e0925320fa721f50ac371d650d9db1a8749fb
AndyJames123 <frasco.4zircon@gmail.com>
2018-12-06 07:40:49 +0800

    first commit

---
+++ FrascoAndy.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <body>
+	  
+     <script type="text/javascript">
+		  
+		  let n = 0;
+		  
+	  
+		  function equals(){
+		    let n1 = document.getElementById("num").value;
+		    document.getElementById("num").value = parseInt(n1) + parseInt(n2);
+		  }
+		    function add(){
+			n = document.getElementById("num").value;
+			document.getElementById("num").value = "+";
+		  }
+		    function minus(){
+			n2 = document.getElementById("num").value;
+			document.getElementById("num").value = "-";
+		  }
+		    function multiplication(){
+			n = document.getElementById("num").value;
+			document.getElementById("num").value = "*";
+		  }
+		    function division(){
+			n = document.getElementById("num").value;
+			document.getElementById("num").value = "/";
+		  }
+
+		  
+	  </script>
+  </body>
+</html>
